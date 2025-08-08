e89dc15ebda976bec7c5a4ebfcf563832122ae12
Mahmoud <mahmoudashrafkhalf@gmail.com>
2025-08-08 21:20:59 +0300

    add member &remove member

--- ITI.OOP_Project/Library.cs
+++ ITI.OOP_Project/Library.cs
@@ -21,5 +21,42 @@ namespace ITI.OOP_Project
             Books = books ?? new List<Book>();
             Members = members ?? new List<Member>();
         }
+        public void AddMember(Member member)
+        { 
+            if(member!=null)
+            {
+                foreach(var m in Members)
+                {
+                    if(m.Id== member.Id)
+                    { 
+                        Console.WriteLine("member is Already in Memberlist");      
+                        return;
+                    }
+                }
+                Members.Add(member);
+
+            }
+            else
+            {
+                Members.Add(member);
+            }
+        }
+        public void removeMember(int memberId)
+        {
+            int v = 0;
+            foreach (Member m in Members)
+            {
+                if (m.Id == memberId)
+                {
+                    Members.Remove(m);
+                    v=1; break;
+                }
+            }
+            if(v == 0)
+            {
+                Console.WriteLine("member is Already not found");
+            }
+          
+        }
     }
 }
